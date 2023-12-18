5b36b1ed17f2d1fe76b86ffcee5ca8aa20c16f17
lgaume <lorisprog@gmail.com>
2023-12-18 18:19:09 +0100

    starting the checking, init map seems to bus_error :(

--- Makefile
+++ Makefile
@@ -21,7 +21,7 @@ INC = inc/
 SRC_PATH = src/
 OBJ_PATH = obj/
 
-SRC = so_long pathfinding utils
+SRC = so_long pathfinding utils checks handling_errors
 SRCS 	= $(addprefix $(SRC_PATH), $(addsuffix .c, $(SRC)))
 OBJ 	= $(patsubst $(SRC_PATH)%.c,$(OBJ_PATH)%.o,$(SRCS))
 
--- src/so_long.c
+++ src/so_long.c
@@ -12,7 +12,7 @@
 
 #include "../inc/so_long.h"
 
-/*static bool	is_ber(const char *av)
+static bool	is_ber(const char *av)
 {
 	char	*s;
 
@@ -20,11 +20,43 @@
 	if (s)
 		return (!ft_strcmp(s, ".ber"));
 	return (false);
-}*/
+}
+
+static t_map init_map(char *s)
+{
+	t_map 	out;
+	int		i;
+	int 	fd;
+	char 	*line;
+
+	//out.path = ft_strjoin("../maps/", s);
+	out.path = s;
+	i = 0;
+	fd = open(out.path, O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		printf("%s\n",line);
+		if (!line)
+			break;
+		out.map[i] = line;
+		i++;
+	}
+	return (out);
+}
 
 int	main(int ac, char **av)
 {
-    (void)ac;
-    (void)av;
+	t_map 	map;
+	//t_res	res;
+    if (ac != 2)
+		exit_errors("There should be only one argument!\n");
+	if (!is_ber(av[1]))
+		exit_errors("Map should be a .ber file!\n");
+	map = init_map(av[1]);
+	(void)map;
+	/*res = check_map(map);
+	if (res.state)
+		exit_errors(res.msg);*/
 	return (0);
 }
